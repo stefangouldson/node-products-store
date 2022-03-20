728f4357988b9d6c23c5e3c68d749ad0583af0e3
Stefan Gouldson <stefangouldson@gmail.com>
2022-03-20 02:14:09 +0000

    save data in JSON file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const customPath = require('../util/path');
+const p = path.join(customPath, 'data', 'products.json');
+
+// Gets products from file
+const getProductsFromFile = cb => {
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +21,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.error(err)
+            });
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb)
     }
 }
